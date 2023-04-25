f786f9a061c87aeb7b486b738a132089b8c9d42f
Camila Molina <molina.neculnir@gmail.com>
2023-04-25 00:17:07 -0400

    fix menu

--- js/main.js
+++ js/main.js
@@ -49,9 +49,46 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when the scroll is greater than 350 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        }
+        else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true //animation repeat
+})
+
+sr.reveal(`.home__data, .products__data, .steps__content, footer__container`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.products__card`, {interval: 100})
+sr.reveal(`.about__img, .testimonial__img`, {origin: 'right'})
+sr.reveal(`.about__data, .testimonial__data`, {origin: 'left'})
